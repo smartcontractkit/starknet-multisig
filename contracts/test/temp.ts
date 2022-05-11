ec2b5194a2affce37163adafe9506a124996ac74
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-05-11 15:13:09 +0300

    Automated submit works

--- contracts/test/temp.ts
+++ contracts/test/temp.ts
@@ -22,81 +22,55 @@ import {
 import { transformCallsToMulticallArrays } from "starknet/dist/utils/transaction";
 import { getSelectorFromName } from "starknet/dist/utils/hash";
 import fs from "fs";
-import { InvokeOptions } from "@shardlabs/starknet-hardhat-plugin/dist/types";
+//import { InvokeOptions } from "@shardlabs/starknet-hardhat-plugin/dist/types";
 import * as starkwareCrypto from "@toruslabs/starkware-crypto";
 
 describe("Multisig with single owner", function () {
   this.timeout(3000_000);
 
-  let acc: Contract;
-
-  const starkKeyPair = ec.genKeyPair();
-  const starkKeyPub = ec.getStarkKey(starkKeyPair);
-
   before(async function () {
-    const compiledAccount = json.parse(
-      fs
-        .readFileSync(
-          "./starknet-artifacts/contracts/account/Account.cairo/Account.json"
-        )
-        .toString("ascii")
-    );
+    const messageHash = starkwareCrypto.pedersen(["0", "8"]);
+
+    const kp = starkwareCrypto.ec.keyFromPrivate([12345]);
+    const publicKey = ec.getStarkKey(kp);
+    const signature = ec.sign(kp, messageHash);
 
     console.log("Deployment Tx - Account Contract to StarkNet...");
-
-    const provider = new Provider({
-      baseUrl: "http://alpha4.starknet.io",
-      feederGatewayUrl: "feeder_gateway",
-      gatewayUrl: "gateway",
-    });
-
-    console.log("start deploy");
 
     const accountContractFactory = await starknet.getContractFactory(
       "contracts/account/Account"
     );
     const accountContract = await accountContractFactory.deploy({
-      owners: [starkKeyPub],
+      owners: [number.toBN(publicKey)],
       confirmations_required: 1,
     });
-    const signer = new Signer(starkKeyPair);
+    //
+    console.log("Deployed account at ", accountContract.address);
 
     const calldata = {
-      user: starkKeyPub, //"1628448741648245036800002906075225705100596136133912895015035902954123957052",
-      //  amount: "4321",
-      //  sig1: "1225578735933442828068102633747590437426782890965066746429241472187377583468",
-      //  sig2: "3568809569741913715045370357918125425757114920266578211811626257903121825123",
+      call_array: [
+        {
+          to: number.toBN(accountContract.address),
+          selector: number.toBN(getSelectorFromName("submit_transaction")),
+          data_offset: 0,
+          data_len: 7,
+        },
+      ],
+      calldata: [
+        number.toBN(
+          "0x03af8252c614c634b9ac563745dbff4b102cbda0fc867ae54cc55348c4892bba"
+        ),
+        number.toBN(getSelectorFromName("set_balance")),
+        1,
+        8,
+        number.toBN(publicKey),
+        number.toBN(signature[0]),
+        number.toBN(signature[1]),
+      ],
+      nonce: 0,
     };
-    var inv: Invocation = {
-      contractAddress: accountContract.address,
-      entrypoint: "doit",
-      //calldata:
-    };
-    /*     signer.signTransaction(
-       [inv], 
 
-    }) */
-
-    //await accountContract.invoke("doit", {});
-    await accountContract.invoke("doit", calldata);
-
-    /*     const accountResponse = await provider.deployContract({
-      constructorCalldata: [1, starkKeyPub, 1],
-      contract: compiledAccount,
-      addressSalt: starkKeyPub,
-    });
-    console.log("wait deploy");
-
-    await provider.waitForTransaction(accountResponse.transaction_hash); */
-    /*     acc = new Contract(
-      compiledAccount.abi,
-      accountResponse.address ?? "",
-      provider
-    ); 
-    
-        const { transaction_hash: transferTxHash } = await acc.__execute__();
-        await defaultProvider.waitForTransaction(transferTxHash);
-        */
+    await accountContract.invoke("__execute__", calldata);
   });
 
   describe(" - submit - ", function () {
